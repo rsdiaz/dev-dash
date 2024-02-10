106853907154eaf30c8a7ceae4045e5e969f0bd1
rsdiaz <robertoserrano83@gmail.com>
2024-02-10 21:09:11 +0100

    add dev tools widget

---
+++ web/src/components/DevTools.tsx
@@ -0,0 +1,36 @@
+import useSWR from "swr";
+
+const fetcher = () =>
+  fetch("http://localhost:4000/tools", { method: "GET" }).then((response) =>
+    response.json()
+  );
+
+const DevTools = () => {
+  const {
+    data: tools,
+    error,
+    isValidating,
+  } = useSWR("http://localhost:4000/tools", fetcher);
+
+  if (error) return <div className="failed">failed to load</div>;
+  if (isValidating) return <div className="Loading">Loading...</div>;
+
+  return (
+    <ul className="flex space-x-8">
+      {tools &&
+        tools.map((tool: any, index: number) => (
+          <li key={index}>
+            <div className="flex gap-1">
+              <div dangerouslySetInnerHTML={{__html: tool.svg}} />
+              <p>{tool.curren_version}</p>
+              <p>
+                -{">"} <span className="text-green-300">{tool.latest_version}</span>
+              </p>
+            </div>
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+export default DevTools;
