9a623526a9014659afadd08f39b9416f622b306d
rsdiaz <robertoserrano83@gmail.com>
2024-02-11 20:08:53 +0100

    add Docker widget

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
 import TopBar from './components/TopBar'
 import BookMarks from './components/BookMarks'
+import DockerWidget from './components/DockerWidget'
 
 function App() {
 
   return (
     <>
-      <TopBar />
+      <div className='mx-11'>
+        <TopBar />
+      </div>
+      <div className='mt-8 mx-11 flex gap-4'>
+        <DockerWidget />
+      </div>
       <main className='max-w-[130rem] mx-auto'>
         <div className='p-20'>
           <BookMarks />
